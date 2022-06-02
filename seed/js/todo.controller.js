a491cdd82f38b9456258fde10dc4a7296058b6ba
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2022-06-02 14:52:32 -0400

    Add new functionality, add, edit, delete

--- seed/js/todo.controller.js
+++ seed/js/todo.controller.js
@@ -1,4 +1,19 @@
 function TodoController() {
+  this.newTodo = "";
+
+  this.removeTodo = function (item, index) {
+    this.list.splice(index, 1);
+  };
+
+  this.addNewTodo = function () {
+    this.list.unshift({
+      title: this.newTodo,
+      completed: false,
+    });
+
+    this.newTodo = "";
+  };
+
   this.list = [
     {
       title: "First todo item!",
